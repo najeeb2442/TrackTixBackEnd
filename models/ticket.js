51a84e2ec0c468de16dfb96fbaec836b136d79d7
nagob <s.najib2442@gmail.com>
2024-03-22 03:47:37 +0300

    done with models

--- models/ticket.js
+++ models/ticket.js
@@ -1,11 +1,21 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-  {},
+  {
+    subject: { type: String, required: true },
+    content: { type: String, required: true },
+    member: { type: Schema.Types.ObjectId, ref: "Role", required: true },
+    priority: { type: String, required: true }, //enum: [""]
+    status: { type: String, required: true }, //enum: ["pending"] and other words
+    createdBy: { type: Schema.Types.ObjectId, ref: "Role", required: true },
+    solvedBy: { type: Schema.Types.ObjectId, ref: "Ticket" },
+    comments: [{ type: String }],
+    attachments: [{ type: String }],
+  },
   {
     timestamps: true,
   }
-)
+);
 
-module.exports = mongoose.model("Ticket", userSchema)
+module.exports = mongoose.model("Ticket", userSchema);
