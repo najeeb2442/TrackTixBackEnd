c86e237ef0deab8528059ffe1a7b7b4dbf5a82ea
nagob <s.najib2442@gmail.com>
2024-03-23 15:56:01 +0300

    roles route and controller done

---
+++ controllers/roles.js
@@ -0,0 +1,77 @@
+const Role = require("../models/role.js")
+const User = require("../models/user.js")
+// const Role = require("../models/role.js")
+
+const index = async (req, res) => {
+  //done
+  try {
+    let roles = await Role.find({ team: req.params.id }).populate([
+      {
+        path: "team",
+        model: "Team",
+      },
+    ])
+    res.json(roles)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+}
+
+const show = async (req, res) => {
+  // done
+  try {
+    const role = await Role.findById(req.params.id).populate("team")
+    res.json(role)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+}
+
+// i am here right now
+
+const newRole = async (req, res) => {
+  //done
+  try {
+    const role = await Role.create(req.body)
+    res.send("Role Created")
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+}
+
+const updateRole = async (req, res) => {
+  try {
+    // req.body.status = true
+    let role = await Role.updateOne({ _id: req.params.id }, req.body)
+    res.json(role)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+}
+
+const deleteRole = async (req, res) => {
+  try {
+    await Role.deleteOne({ _id: req.params.id }).exec()
+    res.json(true)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+}
+
+const assignRole = async (req, res) => {
+  try {
+    await User.updateOne({ _id: req.params.id }, { $push: { roles: req.body } })
+    res.json(true)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+}
+
+module.exports = {
+  deleteRole,
+  updateRole,
+  newRole,
+  index,
+  show,
+  assignRole,
+}
--- models/notification.js
+++ models/notification.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
 const notificationSchema = new Schema(
   {
@@ -10,6 +10,6 @@ const notificationSchema = new Schema(
   {
     timestamps: true,
   }
-);
+)
 
-module.exports = mongoose.model("Notification", notificationSchema);
+module.exports = mongoose.model("Notification", notificationSchema)
